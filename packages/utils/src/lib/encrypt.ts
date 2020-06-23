e2b3d8b197eca9c876d3c03424daa98d90198e8c
Guillermo <guillermo@mantl.com>
2020-06-23 09:32:31 -0400

    fix: remove unnecessary comments

--- packages/utils/src/lib/encrypt.ts
+++ packages/utils/src/lib/encrypt.ts
@@ -2,15 +2,14 @@ import crypto from 'crypto'
 
 /**
  * Encrypt a plain password.
- * @remarks
- *
- * The `draw` member implements the main rendering for a widget.
  *
  * @param password - plain password
- * @returns Promise<string>, Returns a Promise with version hashed of the given password.
  *
- * @beta @virtual
+ * @returns Returns a Promise with version hashed of the given password.
+ *
+ * @beta
  */
-export default async function encrypt(password: string) {
+
+export default async function encrypt(password: string): Promise<string> {
   return crypto.createHash('sha256').update(password).digest('hex')
 }
